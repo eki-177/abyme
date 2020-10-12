673da8b7705bdf5fb80679cdf96fe430c8da2e99
romain sanson <romainsanson.gm@gmail.com>
2020-10-12 20:08:54 +0200

    basic lifecycle events

--- javascript/abyme_controller.js
+++ javascript/abyme_controller.js
@@ -27,14 +27,49 @@ export default class extends Controller {
       html = html.replace(/<template[\s\S]+<\/template>/g, template);
     }
 
+    this.dispatch('abyme:before-add')
     this.associationsTarget.insertAdjacentHTML(this.position, html);
+    this.dispatch('abyme:after-add')
   }
 
   remove_association(event) {
     event.preventDefault();
 
+    this.dispatch('abyme:before-remove')
     let wrapper = event.target.closest('.abyme--fields');
     wrapper.querySelector("input[name*='_destroy']").value = 1;
     wrapper.style.display = 'none';
+    this.dispatch('abyme:before-after')
+  }
+
+  dispatch(type) {
+    const event = new CustomEvent(type, { detail: this })
+    this.element.dispatchEvent(event)
+
+    if (type === 'abyme:before-add') {
+      if (this.beforeAdd) this.abymeBeforeAdd(event)
+    } else if (type === 'abyme:after-add') {
+      if (this.afterAdd) this.abymeAfterAdd(event)
+    } else if (type === 'abyme:before-remove') {
+      if (this.beforeRemove) this.abymeBeforeRemove(event)
+    } else if (type === 'abyme:after-remove') {
+      if (this.afterRemove) this.abymeAfterRemove(event)
+    }
+  }
+
+  abymeBeforeAdd(event) {
+    console.log(event)
+  }
+
+  abymeAfterAdd(event) {
+    console.log(event)
+  }
+
+  abymeBeforeRevome(event) {
+    console.log(event)
+  }
+
+  abymeAfterRemove(event) {
+    console.log(event)
   }
 }
