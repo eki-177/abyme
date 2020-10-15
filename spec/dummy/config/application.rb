0a775ff94902f6bc13d1c101639da40a94d1788f
Louis Sommer <louis.sommer@gmail.com>
2020-10-15 17:25:27 +0200

    installing rails

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,15 +1,11 @@
 require_relative 'boot'
 
 require 'rails'
-require "active_model/railtie"
-# require "active_job/railtie"
-require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
+require 'rails/all'
+# require "active_model/railtie"
+# require "active_record/railtie"
+# require "action_controller/railtie"
+# require "action_view/railtie"
 
 Bundler.require(*Rails.groups)
 require "abyme"
