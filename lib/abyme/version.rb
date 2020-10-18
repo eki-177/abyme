6e0d470bae6177189b7983477ef8ba393de9c6b4
Louis Sommer <louis.sommer@gmail.com>
2020-10-18 16:49:35 +0200

    moved nocov commens

--- lib/abyme/version.rb
+++ lib/abyme/version.rb
@@ -1,11 +1,11 @@
+# :nocov:
 module Abyme
   module VERSION
-    # :nocov:
     MAJOR = 0
     MINOR = 2
     PATCH = 3
 
     STRING = [MAJOR, MINOR, PATCH].join(".")
-    # :nocov:
   end
 end
+# :nocov:
