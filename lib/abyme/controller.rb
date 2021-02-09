55f5f2a790a0c1a055dcc709fbbf5ba4d7484634
Louis Sommer <louis.sommer@gmail.com>
2021-02-09 22:51:20 +0100

    forgot to add most important file

---
+++ lib/abyme/controller.rb
@@ -0,0 +1,14 @@
+module Abyme
+  module Controller
+    def abyme_attributes
+      return [] if resource_class.nil?
+      resource_class.abyme_attributes
+    end
+
+    private
+
+    def resource_class
+      self.class.name.match(/(.*)(Controller)/)[1].singularize.safe_constantize
+    end
+  end
+end
