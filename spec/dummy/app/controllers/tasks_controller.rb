a6972440d96384ce2560ad7c58406da44124eabd
Louis Sommer <louis.sommer@gmail.com>
2021-07-02 11:25:43 +0200

    trying to fix controller generator

--- spec/dummy/app/controllers/tasks_controller.rb
+++ spec/dummy/app/controllers/tasks_controller.rb
@@ -13,16 +13,6 @@ class TasksController < ApplicationController
     @task.save ? redirect_to(tasks_path) : render(:new)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  def update
-    @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to edit_task_path(@task)
-  end
-
   private
 
   def task_params
