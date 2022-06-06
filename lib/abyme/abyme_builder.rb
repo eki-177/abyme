4a52d8baf3524486832d314ddc65224ce3148823
Louis Sommer <louis.sommer@gmail.com>
2022-06-06 17:54:56 +0200

    Ensured passed locals worked with either #abyme_for, #new_records, or #records methods

--- lib/abyme/abyme_builder.rb
+++ lib/abyme/abyme_builder.rb
@@ -24,8 +24,9 @@ module Abyme
     # passing association, form and options to it
 
     def records(options = {})
+      locals = merge_locals(options[:locals])
       persisted_records_for(@association, @form, options) do |fields_for_association|
-        render_association_partial(@association, fields_for_association, @partial, @locals, @context)
+        render_association_partial(@association, fields_for_association, @partial, locals, @context)
       end
     end
 
@@ -34,9 +35,16 @@ module Abyme
     # calls the #new_records_for helper method
     # passing association, form and options to it
     def new_records(options = {}, &block)
+      locals = merge_locals(options[:locals])
       new_records_for(@association, @form, options) do |fields_for_association|
-        render_association_partial(@association, fields_for_association, @partial, @locals, @context)
+        render_association_partial(@association, fields_for_association, @partial, locals, @context)
       end
+    end
+
+    private
+
+    def merge_locals(passed_locals)
+      passed_locals ? @locals.merge(passed_locals) : @locals
     end
   end
 end
